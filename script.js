3eda2583ed672f5f9f55f9b0f980c56f9e0fcda0
Meekaylah <kaylahusanga@gmail.com>
2024-01-24 21:33:49 +0100

    If there is a current song, go to next song

--- script.js
+++ script.js
@@ -109,7 +109,11 @@ const pauseSong = () => {
 
 const playNextSong = () => {
   if (userData?.currentSong === null) {
-    playSong(userData?.songs[0].id)
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1]
+    playSong(nextSong.id)
   }
 }
 
